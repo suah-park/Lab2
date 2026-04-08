d24bd4531828032101689720f678195900e860f8
suahpark <cathy572@snu.ac.kr>
2026-04-08 09:18:04 +0900

    complete lab2 main

--- main.cpp
+++ main.cpp
@@ -9,14 +9,26 @@ vector<int> vec;
 void func(int cnt) {
 
 
+	if (cnt==M){
+	       for (int i=0;i<M;i++){
+	       	cout << vec[i] << " ";}
+	cout<<endl;
+	return;	
+	}
 
-
-
-
-
+	for (int i=1;i<=N;i++){
+		if (vec.empty() || i>vec.back()){
+			vec.push_back(i);
+			func(cnt+1);
+			vec.pop_back();
+		}
+	}
 }
+
+
 
 int main() {
    cin >> N >> M;
    func(0);
+   return 0;
 }
